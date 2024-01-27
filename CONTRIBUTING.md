7088505eec661946abfb33d2cf9e9410ea4fa33c
mnjammy007 <nasir@appanalytics.in>
2024-01-28 01:21:03 +0530

    user can now get register success email.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -13,7 +13,7 @@ docker run -dp 5000:5000 -w /app -v "$(pwd):/app" falsk-smorest-api
 ##  How to run docker app locally
 
 ```
-docker run -dp 5000:5000 -w /app -v "$(pwd):/app" falsk-smorest-api sh -c "flask run"
+docker run -dp 5000:5000 -w /app -v "$(pwd):/app" rest-api-flask sh -c "flask run"
 
 ```
 
--- schemas.py
+++ schemas.py
@@ -46,5 +46,8 @@ class TagAndItemSchema(Schema):
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
-    # password = fields.Str(required=True, load_only=True)
-    password = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+    # password = fields.Str(required=True)
+
+class UserRegisterSchema(UserSchema):
+    email = fields.Str(required=True)
